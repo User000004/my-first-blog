5bd2505893f7af2469f9bb91f765c650fc49f513
User1 <admin@admin.com>
2022-07-05 13:12:21 +0300

    My first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+def myFunction(a):
+  if a == 1:
+    print('Hello 1')
+  elif a == 2:
+    print('Hello 2')
+  else:
+    print('Hello 3')
+myFunction(2)
+myFunction(1)
+myFunction(8)
+
+# user1@user1-System-Product-Name:~/Documents/1$ source myvenv/bin/activate
+# (myvenv) user1@user1-System-Product-Name:~/Documents$ python manage.py runserver
+
